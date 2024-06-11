39fa880d95b53cf236526a6132cec7e61741447b
Marco Alonso <marcoalonsorota@hotmail.com>
2024-06-10 19:22:26 -0600

    Initial Commit

---
+++ CustomCarrouselView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomCarrouselView
+//
+//  Created by Marco Alonso on 10/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomCarrouselView/CustomCarrouselViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCarrouselViewApp.swift
+//  CustomCarrouselView
+//
+//  Created by Marco Alonso on 10/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomCarrouselViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
